d6626936d2790de866242de87b81d52c0f57a972
oshipka <olenkashipka@gmail.com>
2018-11-29 19:35:30 +0200

    add diffi-helman

---
+++ Data protection/Generator.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Windows.Documents;
+
+namespace Data_protection
+{
+	public class Generator
+	{
+		private static int _p;
+		private static int _a;
+		private static int _randA;
+		private static int _randB;
+		private static int _X;
+		private static int _Y;
+		private static int _k;
+		private static int _kp;
+
+		public Generator()
+		{
+			_p = GetRandomPrimeNumber();
+			_a = GetPrimaryRoot(_p);
+			_randA = GetRandomNumber();
+			_randB = GetRandomNumber();
+			_X = CalculateA(_randA);
+			_Y = CalculateA(_randB);
+			_k = CalculateK(_Y, _randA);
+			_kp = CalculateK(_X, _randB);
+		}
+
+		private static int GetRandomPrimeNumber()
+		{
+			var random = new Random();
+			var randomVal = random.Next(1000, 1000000);
+			var result = 6 * randomVal + (randomVal % 2);
+			return result;
+		}
+
+		private static int GetRandomNumber()
+		{
+			var random = new Random();
+			return random.Next(1000, 1000000);
+		}
+
+		private static int GetPrimaryRoot(int val)
+		{
+			for (var a = 1; a < val + 1; a++)
+			{
+				var cnt = 0;
+				for (var k = 1; k < val; k++)
+				{
+					if ((int)Math.Pow(a, k)% val == 1)
+					{
+						cnt += 1;
+					}
+				}
+
+				if (cnt == 1 && (int)Math.Pow(a, val-1)% val == 1)
+				{
+					return a;
+				}
+			}
+			throw new Exception("Could not calculate primary root");
+		}
+
+		private static int CalculateA(int power)
+		{
+			return (int)Math.Pow(_a, power) % _p;
+		}
+
+		private static int CalculateK(int val, int power)
+		{
+			return (int) Math.Pow(val, power) % _p;
+		}
+
+		private static int Phi(int n)
+		{
+			var ret = 1;
+			for (var i = 2; i * i <= n; ++i)
+			{
+				var p = 1;
+				while (n % i == 0)
+				{
+					p *= i;
+					n /= i;
+				}
+
+				if ((p /= i) >= 1) ret *= p * (i - 1);
+			}
+
+			return --n == 1 ? n * ret : ret;
+		}
+
+		public int GetP()
+		{
+			return _p;
+		}
+
+		public int GetA()
+		{
+			return _a;
+		}
+
+		public int GetRandA()
+		{
+			return _randA;
+		}
+
+		public int GetRandB()
+		{
+			return _randB;
+		}
+
+		public int GetX()
+		{
+			return _X;
+		}
+
+		public int GetY()
+		{
+			return _Y;
+		}
+
+		public int GetK()
+		{
+			return _k;
+		}
+
+		public int GetKP()
+		{
+			return _kp;
+		}
+	}
+}
--- Data protection/MainWindow.xaml.cs
+++ Data protection/MainWindow.xaml.cs
@@ -1,127 +1,30 @@
 ﻿using System;
-using System.ComponentModel;
-using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Input;
-using Microsoft.Win32;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace Data_protection
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow
-	{
-		private int[] _key = new int[2];
-		private string _alphabet;
-		private bool _action;
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
 
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
-
-		private void Cipher_Button_Click(object sender, RoutedEventArgs e)
-		{
-			_action = true;
-			KeyInputBox.Visibility = Visibility.Visible;
-		}
-
-		private void Decipher_Button_Click(object sender, RoutedEventArgs e)
-		{
-			_action = false;
-			KeyInputBox.Visibility = Visibility.Visible;
-		}
-
-		private void Ok(object sender, RoutedEventArgs e)
-		{
-
-			KeyInputBox.Visibility = Visibility.Collapsed;
-			try
-			{
-				_key = Utils.GetNumbers(KeyInput.Text);
-			}
-			catch (Exception exception)
-			{
-				KeyInputBox.Visibility = Visibility.Visible;
-				MessageBox.Show(exception.Message + "\n" + exception.StackTrace, "Error", MessageBoxButton.OK,
-					MessageBoxImage.Error);
-			}
-
-			try
-			{
-				if (_action)
-				{
-					OutputBox.Text = Ciphers.Cipher.BBSMethod(InputBox.Text, _key, out var seed);
-					MessageBox.Show("This is your seed. Remember it for the deciphering\n" + seed, "Information", MessageBoxButton.OK,
-						MessageBoxImage.Information);
-				}
-
-				if (!_action)
-				{
-					OutputBox.Text = Ciphers.Decipher.BBSMethod(InputBox.Text, _key);
-				}
-			}
-			catch (Exception exception)
-			{
-				MessageBox.Show(exception.Message + "\n" + exception.StackTrace, "Error", MessageBoxButton.OK,
-					MessageBoxImage.Error);
-			}
-
-
-
-		}
-
-		private void Cancel(object sender, RoutedEventArgs e)
-		{
-			KeyInputBox.Visibility = Visibility.Collapsed;
-			KeyInput.Text = string.Empty;
-		}
-
-		private async void OpenFile(object sender, RoutedEventArgs e)
-		{
-			var dlg = new OpenFileDialog
-			{
-				DefaultExt = ".txt",
-				Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*"
-			};
-
-
-			var result = dlg.ShowDialog();
-
-			if (result != true) return;
-			var filename = dlg.FileName;
-			try
-			{
-				using (var sr = new StreamReader(filename))
-				{
-					var line = await sr.ReadToEndAsync();
-					InputBox.Text = line;
-				}
-			}
-			catch (Exception ex)
-			{
-				InputBox.Text = "Could not read the file: " + ex.Message;
-			}
-		}
-
-		private void SaveFile(object sender, RoutedEventArgs e)
-		{
-			var dlg = new SaveFileDialog
-			{
-				Filter = "Text file (*.txt)|*.txt",
-				InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
-			};
-			if (dlg.ShowDialog() == true)
-				File.WriteAllText(dlg.FileName, OutputBox.Text);
-		}
-
-		private void EnterKeyDown(object sender, KeyEventArgs e)
-		{
-			if (e.Key == System.Windows.Input.Key.Enter)
-			{
-				InputBox.Text = InputBox.Text + "\n";
-			}
-		}
-	}
+        
+    }
 }
